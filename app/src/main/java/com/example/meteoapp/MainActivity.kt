53845209c43f516f4286102406b3a17f5b6dba91
Kaoutar <kaoutarbenameur@gmail.com>
2025-01-13 16:57:40 +0100

    add navbar

--- app/src/main/java/com/example/meteoapp/MainActivity.kt
+++ app/src/main/java/com/example/meteoapp/MainActivity.kt
@@ -1,25 +1,25 @@
 package com.example.meteoapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.meteoapp.Navigation.NavigationGraph
 import com.example.meteoapp.Navigation.Routes
 import com.example.meteoapp.ui.theme.MeteoAppTheme
-import kotlin.math.log
-import kotlin.math.log2
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,17 +28,43 @@ class MainActivity : ComponentActivity() {
         setContent {
             MeteoAppTheme {
                 val navController = rememberNavController()
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
 
-                NavigationGraph(
-                    navController = navController,
-                    modifier = Modifier
-                )
-                NavigationBar {
-                    NavigationBarItem(
-                        selected = false,
-                        onClick = {
-                            Log.d(tag)
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar {
+                            NavigationBarItem(
+                                selected = currentRoute == Routes.HOME_PAGE,
+                                onClick = {
+                                    navController.navigate(Routes.HOME_PAGE) {
+                                        popUpTo(Routes.HOME_PAGE) { inclusive = true }
+                                    }
+                                },
+                                icon = {
+                                    Icon(Icons.Filled.Home, contentDescription = "Accueil")
+                                },
+                                label = { Text("Accueil") }
+                            )
+
+                            NavigationBarItem(
+                                selected = currentRoute == Routes.FAVORIS,
+                                onClick = {
+                                    navController.navigate(Routes.FAVORIS) {
+                                        popUpTo(Routes.FAVORIS) { inclusive = true }
+                                    }
+                                },
+                                icon = {
+                                    Icon(Icons.Filled.Favorite, contentDescription = "Favoris")
+                                },
+                                label = { Text("Favoris") }
+                            )
                         }
+                    }
+                ) { paddingValues ->
+                    NavigationGraph(
+                        navController = navController,
+                        modifier = Modifier.padding(paddingValues)
                     )
                 }
             }
